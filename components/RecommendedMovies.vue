0642878f27cbbc6663f0bcd3d8dadb54dab19977
gbura <rage6999@wp.pl>
2024-07-08 18:24:12 +0200

    test if searching movies will work

--- components/RecommendedMovies.vue
+++ components/RecommendedMovies.vue
@@ -11,8 +11,19 @@
 <script setup lang="ts">
 import database from '../data.json'
 import type { APIResponse } from '~/types/APIResponse'
+const route = useRoute()
+
+// const searchingMovie = ref<string>(route.query.search?.toString() || '')
 
 const db = database as APIResponse[]
 
 const recommendedMovies = db.filter((movie: APIResponse) => !movie.isTrending)
+
+// const filteredMovies = computed(() => {
+// 	const query = searchingMovie.value.toLowerCase()
+// 	if (query) {
+// 		return db.filter((movie: APIResponse) => movie.title.toLowerCase().includes(query))
+// 	}
+// 	return db.filter((movie: APIResponse) => !movie.isTrending)
+// })
 </script>
--- components/SearchInput.vue
+++ components/SearchInput.vue
@@ -1,13 +1,20 @@
 <template>
 	<div class="flex items-center justify-center mt-4 border-b-2 border-gray py-2 lg:ml-4 lg:mt-0">
 		<img src="../assets/images/icon-search.svg" class="w-[30px]" />
-		<input type="text" class="w-full bg-transparent pl-4 outline-none text-white" :placeholder="placeholder" />
+		<input
+			type="text"
+			class="w-full bg-transparent pl-4 outline-none text-white"
+			:placeholder="placeholder"
+			v-model="searchQuery"
+			@input="filterMovies" />
 	</div>
 </template>
 
 <script setup lang="ts">
 const route = useRoute()
+const router = useRouter()
 const placeholder = ref<string>('')
+const searchQuery = ref<string>('')
 
 const updatePlaceholder = () => {
 	if (route.name === 'bookmarks') {
@@ -16,5 +23,14 @@ const updatePlaceholder = () => {
 		placeholder.value = 'Search for movies or TV series'
 	}
 }
+
+const filterMovies = () => {
+	router.push({
+		query: {
+			search: searchQuery.value
+		},
+	})
+}
+
 onMounted(() => updatePlaceholder())
 </script>
